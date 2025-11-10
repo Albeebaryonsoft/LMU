3b49828b36eee5547ae42ff0449a0f9cc40d77b0
Albee <qqqqiuyanhang@gmail.com>
2025-11-10 09:03:36 +0100

    changes

--- code/Little_tricks.R
+++ code/Little_tricks.R
@@ -4,9 +4,29 @@ run_app()  # useful for bias evaluation
 
 
 qt(1 - 0.05/2, df = 19) 
+qt(0.05/2, df = 93) 
+
+qt(0.05/2, df = 166) 
+
+
+1 - pt(2.99, df = 19)
+pnorm(2.99)
+dpois(2,21)
+ppois(2,21)
+
+
+2*pt((1242.2-1286)/(256.2/sqrt(94)), df = 93)
+
+qt(0.05/2, df = 93)  # t=1.985
 qnorm(0.05/2)
+
 qnorm(1 - 0.05/2)
 1 - pt(2.98, df = 19)
+
+1 - pt(2.98, df = 19)
+
+pt(7.72, df = 166)
+qt(0.05/2, df = 166)  # t=1.985
 
 dpois(9,3) #概率质量函数 (PMF)：计算 P(X = x)
 ppois(9,3) # cumulative 
@@ -20,7 +40,12 @@ p <- n / 6
 likelihood <- dbinom(7, size = 10, prob = p)  
 data.frame(n, p, likelihood)  
 
-# when n=4, p=4/6, largest likelihood
+
+
+# two-sample t test
+t.test()
+
+
 
 
 
